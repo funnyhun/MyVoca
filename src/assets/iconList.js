cf07b81519a0d5d54f2fd5902e5d4657dc2412b5
funnyhun <funnyhun0222@gmail.com>
2025-12-02 21:08:37 +0900

    feat : 아이콘 추가 1) AccountIcon, HomeIcon, PlayIcon, WordIcon 추가

---
+++ src/assets/iconList.js
@@ -0,0 +1,8 @@
+import AccountIcon from "./icon/account.svg?react";
+
+import HomeIcon from "./icon/home.svg?react";
+import PlayIcon from "./icon/play.svg?react";
+import WordIcon from "./icon/word.svg?react";
+
+export { AccountIcon };
+export { HomeIcon, PlayIcon, WordIcon };
