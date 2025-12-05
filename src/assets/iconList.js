5fb75ea3863ee42fae7bc663c1005e1939acc13d
funnyhun <funnyhun0222@gmail.com>
2025-12-05 19:35:00 +0900

    style : 아이콘 추가 1) Word, Play 컴포넌트 아이콘 추가

--- src/assets/iconList.js
+++ src/assets/iconList.js
@@ -1,13 +1,23 @@
 import AccountIcon from "./icon/account.svg?react";
 
+// layout
 import QuizIcon from "./icon/quiz.svg?react";
 import LeftIcon from "./icon/left.svg?react";
 import RightIcon from "./icon/right.svg?react";
 
+// navigation
 import HomeIcon from "./icon/home.svg?react";
 import PlayIcon from "./icon/play.svg?react";
 import WordIcon from "./icon/word.svg?react";
 
+// pages/play
+import SpeakIcon from "./icon/speak.svg?react";
+
+import DayIcon from "./icon/day.svg?react";
+import DoneIcon from "./icon/done.svg?react";
+
 export { AccountIcon };
 export { QuizIcon, LeftIcon, RightIcon };
 export { HomeIcon, PlayIcon, WordIcon };
+export { SpeakIcon };
+export { DayIcon, DoneIcon };
