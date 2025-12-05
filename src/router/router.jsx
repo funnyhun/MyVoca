207d71678e6d4b9858a5cffb1861fee9404c464a
funnyhun <funnyhun0222@gmail.com>
2025-12-05 19:39:28 +0900

    feat : Play 컴포넌트 라우팅 추가 1) 라우팅 객체에 Play 컴포넌트를 추가했어요. 2) 초기 접속 시 "/home" 으로 이동하도록 설정했어요.

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom"
 
 import { App } from "../App";
 import { Home } from "../pages/Home/Home";
+import { Play } from "../pages/Play/Play";
+
 import { HomeIcon, PlayIcon, WordIcon } from "../assets/iconList";
 
 export const pages = [
   { path: "/home", element: <Home />, name: "홈", icon: <HomeIcon /> },
-  { path: "/play", element: <Home />, name: "시작하기", icon: <PlayIcon /> },
+  { path: "/play", element: <Play />, name: "시작하기", icon: <PlayIcon /> },
   { path: "/word", element: <Home />, name: "단어장", icon: <WordIcon /> },
 ];
 
@@ -15,7 +17,7 @@ const routes = [
     path: "/",
     element: <App />,
     name: "App",
-    children: pages,
+    children: [{ index: true, element: <Navigate to="/home" replace /> }, ...pages],
   },
   {
     path: "*",
