a924d914869328e9edbd492d3b898057ccc07665
funnyhun <funnyhun0222@gmail.com>
2025-12-06 21:22:02 +0900

    deploy : mvp_0.0

--- assets/index-CyBSAcUx.js
+++ assets/index-CyBSAcUx.js
@@ -194,7 +194,7 @@ Please change the parent <Route path="${Q}"> to <Route path="${Q==="/"?"*":`${Q}
   z-index: 999;
 
   width: 100%;
-  height: calc(3rem + env(safe-area-inset-bottom));
+  height: calc(3.5rem + env(safe-area-inset-bottom));
   background-color: ${({theme:n})=>n.main};
 
   display: flex;
