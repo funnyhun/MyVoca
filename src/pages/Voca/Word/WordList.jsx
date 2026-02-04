ed6cf09cbcab85ef14a0d564edb2cb6ea6f0a6b8
funnyhun <funnyhun0222@gmail.com>
2026-02-04 19:03:23 +0900

    feat : Voca 서비스 내 세부 조회 기능 개발 1) Voca/WordList 에서 학습 데이터를 선택한 뒤 단어의 세부조회가 가능해요. 2) 세부조회 시 데이터 필터 및 검색 기능이 사용 가능해요. 3) 현재 mock-data 기반으로 구현됐어요.
    refs : #6

--- src/pages/Voca/Word/WordList.jsx
+++ src/pages/Voca/Word/WordList.jsx
@@ -1,20 +1,32 @@
 import styled from "styled-components";
+import { useState, useMemo, useCallback } from "react";
 
 import { useSelected } from "../../../hooks/useMyParam";
 import { useWord } from "../../../hooks/useWord";
+
 import { WordItem } from "./WordItem";
 import { WordSearch } from "./WordSearch";
+import { WordFilter } from "./WordFilter";
+import { WordNoResult } from "./WordNoResult";
+import { WordEmpty } from "./WordEmpty";
 
-const Wrapper = styled.ul`
+import { FILTER_SET, FILTER_TYPE } from "./filter";
+
+const Wrapper = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+
+  padding-top: 1rem;
+`;
+
+const Content = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 1rem;
 
   overflow-y: auto;
-
-  & > :first-child {
-    margin-top: 1rem;
-  }
 
   & > :last-child {
     margin-bottom: 1rem;
@@ -23,17 +35,36 @@ const Wrapper = styled.ul`
 
 export const WordList = () => {
   const { selected } = useSelected();
-  const { words } = useWord(selected);
+  const { words = [] } = useWord(selected);
 
-  console.log(words);
+  const [filterType, setFilterType] = useState(FILTER_TYPE[0]);
+  const [keyword, setKeyword] = useState("");
+
+  const clearCondition = () => {
+    setFilterType(FILTER_SET[0]);
+    setKeyword("");
+  };
+
+  const filteredWords = useMemo(() => {
+    const filter = FILTER_SET[filterType].callback;
+    return filter(words);
+  }, [words, filterType]);
+
+  const renderContentUI = useCallback(() => {
+    if (words.length === 0) return <WordEmpty />;
+
+    if (filteredWords.length === 0) return <WordNoResult />;
+
+    return filteredWords.map((word) => {
+      return <WordItem word={word} key={word.id} />;
+    });
+  }, [words, filteredWords]);
 
   return (
     <Wrapper>
       <WordSearch />
-      <WordItem word={words[0]} />
-      {words.map((word) => {
-        return <WordItem word={word} key={word.id} />;
-      })}
+      <WordFilter currentFilter={filterType} setFilterType={setFilterType} />
+      <Content>{renderContentUI()}</Content>
     </Wrapper>
   );
 };
